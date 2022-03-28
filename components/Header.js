bf973ed28c2122513a5d3aba8e348a1944d403ab
Victor Romero <vromerocomino@gmail.com>
2022-03-28 14:00:14 +0200

    setup tailwindcss

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <nav className='flex justify-between'>
+      <div>
+        <span>next</span><span className="font-bold">xkcd</span>
+      </div>
+      <ul>
+      </ul>
+    </nav>
+  )
+}
---
+++ components/PageLayout.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+import styles from '../styles/Home.module.css'
+
+export default function PageLayout ({
+  children, 
+  title = 'nextXKCD', 
+  description = 'Comic app with Next.js'
+}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {children}
+    </div>
+  )
+}
